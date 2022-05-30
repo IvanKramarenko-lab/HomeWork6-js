3ae4870040d52d12fb79ad4d95ad5cf12e1d0322
santakratos <santakratos10@gmail.com>
2022-05-30 18:20:53 +0300

    del while, added for

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let i = 0;
-alert(i);
-
-while (true) {
+for (let i = 0; ; ++i) {
     const number = prompt('Enter your word');
     if (number === 'break') {
         break;
     } else if (number === 'continue') {
-        ++i;
         continue;
+    } else {
+        alert(i);
     }
 }
